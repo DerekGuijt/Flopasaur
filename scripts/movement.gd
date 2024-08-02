7ed9d8f83cdf08e7a13983c5029c5be722785caa
Shane Bell <being.for.itself@gmail.com>
2024-08-02 19:34:24 -0400

    Fighting with collision.

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -4,16 +4,18 @@ extends Node2D
 const speed = 500
 
 func _ready():
-    print_debug("Ding!")
+	print_debug("Ding!")
 
 
 func _process(delta):
-    if Input.is_action_pressed("move_up"):
-        self.position.y -= speed * delta
-    elif Input.is_action_pressed("move_down"):
-        self.position.y += speed * delta
-    
-    if Input.is_action_pressed("move_left"):
-        self.position.x -= speed * delta
-    elif Input.is_action_pressed("move_right"):
-        self.position.x += speed * delta
+	if Input.is_action_pressed("move_up"):
+		self.position.y -= speed * delta
+	elif Input.is_action_pressed("move_down"):
+		self.position.y += speed * delta
+	
+	if Input.is_action_pressed("move_left"):
+		self.position.x -= speed * delta
+	elif Input.is_action_pressed("move_right"):
+		self.position.x += speed * delta
+
+
